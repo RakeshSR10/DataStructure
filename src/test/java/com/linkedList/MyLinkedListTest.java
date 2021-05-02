dc7dc8c09d5f0404f4f9679960d9abbbaa987ac1
RakeshSR10 <rakeshsrking@gmail.com>
2021-05-02 09:45:28 +0530

    Added: Ability to append to the LinkedList

--- src/test/java/com/linkedList/MyLinkedListTest.java
+++ src/test/java/com/linkedList/MyLinkedListTest.java
@@ -20,4 +20,20 @@ public class MyLinkedListTest {
                 myLinkedList.tail.equals(myFirstNode);
         Assertions.assertTrue(result);
     }
+    //UC3 Append to the LinkedList
+    @Test
+    public void given3NumbersWhenAppendingShouldBeAddedToLast(){
+        MyNode<Integer> myFirstNode = new MyNode<>(56);
+        MyNode<Integer> mySecondNode = new MyNode<>(30);
+        MyNode<Integer> myThirdNode = new MyNode<>(70);
+        MyLinkedList myLinkedList = new MyLinkedList();
+        myLinkedList.add(myFirstNode);
+        myLinkedList.append(mySecondNode);
+        myLinkedList.append(myThirdNode);
+        myLinkedList.printMyNodes();
+        boolean result = myLinkedList.head.equals(myFirstNode) &&
+                myLinkedList.head.getNext().equals(mySecondNode) &&
+                myLinkedList.tail.equals(myThirdNode);
+        Assertions.assertTrue(result);
+    }
 }
