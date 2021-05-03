e0a47969143b7c8fab3be620d366134f1aee6e8b
RakeshSR10 <rakeshsrking@gmail.com>
2021-05-03 08:00:44 +0530

    Added: Deleting FIFO element from Queue

--- src/test/java/com/queue/MyQueueTest.java
+++ src/test/java/com/queue/MyQueueTest.java
@@ -18,4 +18,19 @@ public class MyQueueTest {
         myQueue.append(myThirdNode);
         myQueue.printQueue();
     }
+    //UC2 Ability to Dequeue first element = 56
+    @Test
+    public void given3NumbersInQueueWhenDequeuedShouldMatchWithLastAddedNode(){
+        MyQueue myQueue = new MyQueue();
+        MyNode<Integer> myFirstNode = new MyNode<>(56);
+        MyNode<Integer> mySecondNode = new MyNode<>(30);
+        MyNode<Integer> myThirdNode = new MyNode<>(70);
+        myQueue.Enqueue(myFirstNode);
+        myQueue.append(mySecondNode);
+        myQueue.append(myThirdNode);
+        myQueue.printQueue();
+        INode dequeue = myQueue.Dequeue();
+        myQueue.printQueue();
+        Assertions.assertEquals(myFirstNode,dequeue);
+    }
 }
